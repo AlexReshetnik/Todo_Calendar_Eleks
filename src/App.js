15f08767448df296a9da27de4c0dc9be539790b7
AlexReshetnik <reshetnik262@gmail.com>
2023-05-06 12:58:38 +0300

    first attempt at adaptive design

--- src/App.js
+++ src/App.js
@@ -10,22 +10,29 @@ function App() {
     JSON.parse(localStorage.getItem('sizes')) || ['20%', 'auto']
   );
   localStorage.setItem('sizes', JSON.stringify(sizes));
- 
 
   useEffect(() => {
     window.onresize = onResize;
-    onResize()
+    onResize();
   }, []);
 
   function onResize() {
-    console.log(window.innerWidth);
+    // console.log(window.innerWidth);
     if (window.innerWidth < 576) {
-     
       setSizes(['100%', '0']);
     } else {
-      setSizes(['20%', '70%']);
+      setSizes(['30%', '70%']);
     }
   }
+  const [isPortrait, setOrientation] = useState(
+    window.screen.orientation.type.includes('portrait')
+  );
+  window.screen.orientation.onchange = e => {
+    setOrientation(e.target.type.includes('portrait'));
+  };
+  console.log(window.innerWidth, isPortrait);
+  //portrait//вертикальне
+  //landscape//горизонтальне
 
   const layoutCSS = {
     height: '100%',
@@ -37,14 +44,18 @@ function App() {
   return (
     <div className='App'>
       <div className='container'>
-        <SplitPane sizes={sizes} onChange={setSizes}>
-          <Pane initialSize='30%' minSize='200px' maxSize='100%'>
-            <TodoListComponent style={{...layoutCSS, background: '#ddd'}} />
-          </Pane>
-          <Pane initialSize='70%' minSize='0%' maxSize='100%'>
-            <Calendar style={{...layoutCSS, background: '#ddd'}} />
-          </Pane>
-        </SplitPane>
+        {window.innerWidth < 576 && isPortrait ? (
+          <TodoListComponent style={{...layoutCSS, background: '#ddd'}} />
+        ) : (
+          <SplitPane sizes={sizes} onChange={setSizes}>
+            <Pane initialSize='30%' minSize='200px' maxSize='100%'>
+              <TodoListComponent style={{...layoutCSS, background: '#ddd'}} />
+            </Pane>
+            <Pane initialSize='70%' minSize='0%' maxSize='100%'>
+              <Calendar style={{...layoutCSS, background: '#ddd'}} />
+            </Pane>
+          </SplitPane>
+        )}
       </div>
     </div>
   );
